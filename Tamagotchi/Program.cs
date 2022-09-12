4f7ae7db450b9539625c9e0f3007239b6a5a3c51
Hilda <glimrot.hilda@elev.ga.ntig.se>
2022-09-12 13:38:58 +0200

    ytrjnhtfhntfnhtf

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Tamagotchi tama = new Tamagotchi();
+
+tama.teach("häst");
+tama.PrintStats();
+tama.feed();
+tama.PrintStats();
+
+Console.ReadLine();
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -4,16 +4,72 @@ public class Tamagotchi
 {
     //private
     private string name = "xX_kawaiiNekoXx";
-    private int hunger = 0;
+    private int hunger = 9;
     private int boredom;
-    private bool isAlive;
+    private bool isAlive = true;
     private List<string> words = new List<string>();
 
     private Random generator = new Random();
 
-//public
-    public void feed();
-    public void Hi();
-    public void teach();
+    private int hp;
+
+    public Tamagotchi()
+    {
+        hp = generator.Next(10, 20);
+    }
+
+    //public
+
+    public void feed()
+    {
+        hunger--;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
+    }
+
+
+
+    public void Hi()
+    {
+
+    }
+
+    public void teach(string word)
+    {
+        words.Add(word);
+    }
+
+    public void tick()
+    {
+        hunger++;
+        if (hunger > 10)
+        {
+            isAlive = false;
+        }
+        boredom++;
+        if (boredom > 10)
+        {
+            isAlive = false;
+        }
+
+
+    }
+
+    //Printar ut tamagochins stats
+    public void PrintStats()
+    {
+        Console.WriteLine($"Hunger: {hunger}");
+        Console.WriteLine($"Boredom: {boredom}");
+        if (isAlive == true)
+        {
+            Console.WriteLine("Den lever! :D");
+        }
+        else
+        {
+            Console.WriteLine("Den dog ):");
+        }
+    }
 
 }
